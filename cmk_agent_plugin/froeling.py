be2f31934d59941f5bc5356e8cb3fea2ce60385c
Johannes Link <hannes1977@gmail.com>
2021-12-15 16:55:28 +0100

    Work in progress
    Add multiple checks in CheckMK
    Display Status message correctly with State OK
    TODO: Add metrics and check results based on sensor unit

--- cmk_agent_plugin/froeling.py
+++ cmk_agent_plugin/froeling.py
@@ -1,19 +1,40 @@
 from .agent_based_api.v1 import *
 
-def discover_froeling_status(section):
-    yield Service()
-
-def check_froeling_status(section):
+def discover_froeling_sensors(section):
     for line in section:
         desc, value, sensor_id, factor, unit, total = line[0].split(';')
         if int(sensor_id) == 1:
+            yield Service(item="Status")
+        yield Service(item=desc)
+
+def check_froeling_sensors(item, section):
+    for line in section:
+        desc, value, sensor_id, factor, unit, total = line[0].split(';')
+        if int(sensor_id) == 1 and item == "Status":
             yield Result(state=State.OK, summary=desc)
+            return
+        if desc == item:
+            s = State.OK
+            yield Result(
+                state = s,
+                summary = f"{total}"
+            )
             return
     yield Result(state=State.CRIT, summary="No status message found")
 
+def check_froeling_temps(item, section):
+   for line in section:
+        desc, value, sensor_id, factor, unit, total = line[0].split(';')
+        if desc == item:
+            s = State.OK
+            yield Result(
+                state = s,
+                summary = f"{total}"
+            )
+
 register.check_plugin(
     name="froeling",
-    service_name="Fröling Status",
-    discovery_function=discover_froeling_status,
-    check_function=check_froeling_status,
+    service_name="Fröling %s",
+    discovery_function=discover_froeling_sensors,
+    check_function=check_froeling_sensors,
 )
